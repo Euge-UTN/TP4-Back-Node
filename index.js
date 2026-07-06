4839bc78357b1dc747193aaec02a053cae2d3dc9
Eugenia <eugeniafer2005@gmail.com>
2026-07-06 11:33:19 -0300

    cambios en index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
+
 import gastosRouter from './routes/gastos.js';
 import categoriasRouter from './routes/categorias.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use(morgan('tiny'));
 
 app.use('/api/gastos', gastosRouter);

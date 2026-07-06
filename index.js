9271177b0f7e918af6edc7f298482910c48f8184
Eugenia <eugeniafer2005@gmail.com>
2026-07-06 11:21:09 -0300

    Edite el index.js

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 import express from 'express';
-import morgan from 'morgan'; 
-import { gastos } from './datos.js';
-import gastosRouter from './routes/gasto.js';
+import morgan from 'morgan';
+import gastosRouter from './routes/gastos.js';
 import categoriasRouter from './routes/categorias.js';
 
 const app = express();
 
-app.use(express.json()); 
+app.use(express.json());
 
-app.use(morgan('tiny')); 
+app.use(morgan('tiny'));
 
 app.use('/api/gastos', gastosRouter);
 app.use('/api/categorias', categoriasRouter);
 
-app.get('/info', (req, res) => {
-  const cantidad = gastos.length;
-  const fecha = new Date();
-  
-  res.send(`
-    <p>La API tiene registrado ${cantidad} gasto${cantidad !== 1 ? 's' : ''}</p>
-    <p>${fecha}</p>
-  `);
+// Middleware para rutas no encontradas
+app.use((req, res) => {
+  res.status(404).json({
+    error: 'Ruta no encontrada'
+  });
 });
 
 const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
